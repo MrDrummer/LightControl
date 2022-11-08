d480dfa96ef0dc4ed455140d2e0aaf01b7ee6781
MrDrummer <mrdrummer.programming@hotmail.com>
2022-11-08 20:12:49 +0000

    Move signal R init logic

--- LightControl/LightControl/ClientApp/src/app/app.component.ts
+++ LightControl/LightControl/ClientApp/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {SignalrService} from "./services/signalr.service";
 
 @Component({
   selector: 'app-root',
@@ -8,16 +6,4 @@ import {SignalrService} from "./services/signalr.service";
 })
 export class AppComponent {
   title = 'app';
-  constructor(public signalRService: SignalrService, private http: HttpClient) { }
-  ngOnInit() {
-    this.signalRService.startConnection();
-    this.signalRService.receivedLightControlData();
-    // this.startHttpRequest();
-  }
-  // private startHttpRequest = () => {
-  //   this.http.get('https://localhost:5001/api/chart')
-  //     .subscribe(res => {
-  //       console.log(res);
-  //     })
-  // }
 }
