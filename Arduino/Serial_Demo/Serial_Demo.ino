e6161b95b7bcc1d2b486477b3365cb7082af99fa
MrDrummer <mrdrummer.programming@hotmail.com>
2022-11-11 21:43:57 +0000

    Kinda works

--- Arduino/Serial_Demo/Serial_Demo.ino
+++ Arduino/Serial_Demo/Serial_Demo.ino
@@ -1,19 +1,37 @@
 int ledPin = 13; //integrated LED
 int serialData = '1';
+String readString;
 
 void setup() {
   Serial.begin(9600);  //initialize serial comms
   pinMode(ledPin, OUTPUT);
+  Serial.println("Enter LED Number 0 to 7 or 'x' to clear");
 }
 
 void loop() {
-  if (Serial.available()) {
-    serialData = Serial.read();
-    if(serialData == '1'){
-      digitalWrite(ledPin, HIGH);
-    }
-    else {
-      digitalWrite(ledPin, LOW);
-    }
+
+  while (Serial.available()) {
+    delay(2);  //delay to allow byte to arrive in input buffer
+    char c = Serial.read();
+    readString += c;
   }
+
+  if (readString.length() >0) {
+    Serial.println(readString);
+
+    readString="";
+  } 
+  // if (Serial.available()) {
+  //   serialData = Serial.read();
+  //   // Serial.println('I received: ');
+  //   Serial.println(serialData, DEC);
+  //   if(serialData == '1'){
+  //     digitalWrite(ledPin, HIGH);
+  //     // Serial.println('A 1');
+  //   }
+  //   else {
+  //     digitalWrite(ledPin, LOW);
+  //     // Serial.println('Not a 1');
+  //   }
+  // }
 }
